1638550b394da22f4eb395af9dc583b024e5b184
bufanda <thorsten@bufanda.de>
2025-03-05 15:14:25 +0100

    template for bot command class

---
+++ command/bot.py
@@ -0,0 +1,16 @@
+"""
+    @Author: Thorsten liepert <thorsten@liepert.dev>
+    @Date: 18.02.2025
+    @CLicense: MIT
+    @Description: handler for bot commands
+"""
+from modules.datamanager import ScumLogDataManager
+from command.base import Command
+
+# pylint: disable=too-few-public-methods, too-many-branches, line-too-long
+class BotMangement(Command):
+    """Class to handle command for managing the bot"""
+
+    def someMethod(self) -> None:
+        """some method"""
+        pass
